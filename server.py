4c0aacbce31c4445c7271b8c32dbe7ffc40eb29c
Elina <elina.vankempen@gmail.com>
2024-06-16 07:24:57 -0700

    added compute Ms function

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 import socket
 import pickle
 import numpy as np
+import csv
+
+d = 2  # Adjust this as needed
+n = 3  # Adjust this as needed
+total_clients = d * n
 
 # Function to handle receiving sums from the original server
 def receive_sums_from_server(host='0.0.0.0', port=12346):
@@ -33,12 +38,34 @@ def receive_sums_from_server(host='0.0.0.0', port=12346):
         print(sums_first)
         print("Sum Second:")
         print(sums_second)
-
+        return sums_first, sums_second
     except Exception as e:
         print(f"Error receiving sums: {e}")
     finally:
         client_socket.close()
         server.close()
 
+def computeMs(ks, xs, kprimes, ys):
+    ms = {}
+    k = 1 #change this value
+    k1 = sum(ks[1])
+    k1prime = sum(kprimes[1])
+    for j in range(1,n+1):
+        for i in range(1,d+1):
+            m = xs[i,j]-k1+ks[1,j]-ks[i,j]
+            y_test = k1prime-kprimes[1,j]+kprimes[i,j]+k*m
+            if ys[i,j] == y_test:
+                ms["f{i},{j}"]=m
+            else:
+                print("checksum not verified")
+                return -1
+    return ms
+
 if __name__ == "__main__":
-    receive_sums_from_server()
+    xs, ys = receive_sums_from_server()
+    #ks = np.array(list(csv.reader(open("ks.csv"))))
+    #kprimes = np.array(list(csv.reader(open("kprimes.csv"))))
+    ks = np.genfromtxt("ks.csv",delimiter=",")
+    kprimes = np.genfromtxt("kprimes.csv", delimiter=",")
+    ms = computeMs(ks, xs, kprimes, ys)
+    print("ok")
---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+python client.py 1 1 150 300
+python client.py 1 2 150 300
+python client.py 1 3 150 300
+python client.py 2 1 150 300
+python client.py 2 2 150 300
+python client.py 2 3 150 300
