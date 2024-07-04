2372a2772d92bfac27834c51df5db362415ae634
Elina <elina.vankempen@gmail.com>
2024-07-04 14:55:10 -0700

    testing times running

--- benchmark.sh
+++ benchmark.sh
@@ -4,16 +4,18 @@
 rm bench.csv
 rm aggTimes.csv
 
-for n in {5,10,25,50,100,200,500,1000}; do
-	pkill -f agg.py
-        pkill -f client.py
-	pkill -f server.py
-	echo -n "$n," >> bench.csv
-	echo -n "$n," >> aggTimes.csv
-	./test.sh $n
-        echo "" >> bench.csv
-        echo "" >> aggTimes.csv
-	sleep 2
+for i in {1..5}; do
+	for n in {5,10,25,50,100,200,500,1000}; do
+		pkill -f agg.py
+        	pkill -f client.py
+		pkill -f server.py
+		echo -n "$n," >> bench.csv
+		echo -n "$n," >> aggTimes.csv
+		./test.sh $n
+	        echo "" >> bench.csv
+        	echo "" >> aggTimes.csv
+		sleep 2
+	done
 done
 
 pkill -f agg.py
