60f520daf5006e584fdeae5f00633175ac68b52b
Elina <elina.vankempen@gmail.com>
2024-07-04 14:55:33 -0700

    and this file too

--- test.sh
+++ test.sh
@@ -9,8 +9,8 @@ d=5
 
 for ((i=0;i<d;i++)); do
 	for ((j=0;j<n;j++)); do
-                r1=$(( RANDOM % 1001 ))
-                r2=$(( RANDOM % 1001 ))
+                r1=$(( RANDOM ))
+                r2=$(( RANDOM ))
                 echo -n "$r1," >> ks.csv
                 echo -n "$r2," >> kprimes.csv
 	done
@@ -23,9 +23,9 @@ gnome-terminal -- bash -c "python agg.py $d $n; sleep 1; exit"&
 TERMINAL_PID=$!
 sleep 1
 for ((j=0;j<n;j++)); do
-	g=$(( RANDOM % 1001 ))
+	g=$(( RANDOM ))
 	for ((i=0;i<d;i++)); do
 		python client.py $i $j $g
-		sleep 0.1
+		#sleep 0.1
 	done
 done
