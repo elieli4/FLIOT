d7cc51af34eb2cf16e467b747a06ee3eefc2cf9b
elieli4 <97131658+elieli4@users.noreply.github.com>
2024-09-25 10:22:23 -0700

    Update agg.py up to handle client

--- agg.py
+++ agg.py
@@ -45,12 +45,14 @@ def send_sums(sums_first, sums_second, host='127.0.0.1', port=12346):
         
         print("Sums sent to main server.")  # Debug print
 
-        # Measure how many bytes were sent, and store the info in a file
-        singleSum=len(pickle.dumps((sums['first_sum'],sums['second_sum'])))
+        # Measure how many bytes were sent
         snt = snt + len(sums_serialized)
-        file = open("singleSum.csv","a")
-        file.write(str(singleSum) + "\n")
-        file.close()
+
+        # To measure baselines if needed
+        #singleSum=len(pickle.dumps((sums['first_sum'],sums['second_sum'])))
+        #file = open("singleSum.csv","a")
+        #file.write(str(singleSum) + "\n")
+        #file.close()
 
     except Exception as e:
         print(f"Error sending sums to main server: {e}")
@@ -58,14 +60,14 @@ def send_sums(sums_first, sums_second, host='127.0.0.1', port=12346):
         client.close()
         receive_h()
 
-# Function to compute s_i,j
+# Function to compute the sums to send to the server, x^(i,j) and y^(i,j)
 def compute_sums():
+
+    # First row sums, computed here for efficiency.
     s1_first_sum = sums['first_sum']
     s1_second_sum = sums['second_sum']
     
-    # Initialize 2D arrays for storing sums
-   # sums_first = np.zeros((d, n))
-    #sums_second = np.zeros((d, n))
+    # Initialize arrays for storing the sums. sums_first is the sums of x, sums_second is the sums of y in the protocol
     sums_first = [[0 for _ in range(n)] for _ in range(d)]
     sums_second = [[0 for _ in range(n)] for _ in range(d)]
 
@@ -73,34 +75,42 @@ def compute_sums():
 
     start = time.time()
     for i in range(0, d):
-        if i==0:
-            stt=time.time()
+        #for baseline
+        #if i==0:
+            #stt=time.time()
         for j in range(0, n):
-#            print(f"Checking i={i}, j={j}")  # Debug print
+            # dictionary key for entry in first row and column j, or row i and column j
             key_1j = f"(0, {j})"
             key_ij = f"({i}, {j})"
             if key_1j in received_values and key_ij in received_values:
+                # Encrypted messages retrieved from dictionaries
                 m_1j_first, m_1j_second = received_values[key_1j]
                 m_ij_first, m_ij_second = received_values[key_ij]
+
+                # Swap value of the first row to the value in row i. See protocol.
                 s_ij_first = (s1_first_sum - m_1j_first + m_ij_first)%p
                 s_ij_second = (s1_second_sum - m_1j_second + m_ij_second)%p
+
+                # Store in array
                 sums_first[i][j] = s_ij_first
                 sums_second[i][j] = s_ij_second
- #               print(f"s_{i},{j}_first: {s_ij_first}, s_{i},{j}_second: {s_ij_second}")  # Debug print
-        if i==0:
-            enn = time.time()
-            ff = str(enn-stt) + "\n"
-            file = open("singleAggTimes.csv", "a")
-            file.write(ff)
-            file.close()
+                
+ #              print(f"s_{i},{j}_first: {s_ij_first}, s_{i},{j}_second: {s_ij_second}")  # Debug print
+        #for baseline
+        #if i==0:
+            #enn = time.time()
+            #ff = str(enn-stt) + "\n"
+            #file = open("singleAggTimes.csv", "a")
+            #file.write(ff)
+            #file.close()
+
+    # Measure execution time and write to file
     end=time.time()
-#    print(type(s1_first_sum))
     ti = str(end-start) +","
     file = open("aggTimes.csv", "a")
     file.write(ti)
     file.close
     print(ti)
-    #single sume compute time:
 
     # Send the computed sums to the main server
     send_sums(sums_first, sums_second)
